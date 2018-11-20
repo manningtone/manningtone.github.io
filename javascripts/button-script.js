f26c7aa315267da472ec99ab5da671c8fda55d18
manningtone <ewanmannington@live.co.uk>
2018-11-20 13:58:42 +0000

    Add files via upload

---
+++ javascripts/button-script.js
@@ -0,0 +1,1 @@
+$(".dform button.form-submit").after( "<a href='https://applyforanaccount.cityindex.co.uk/ciuk/sbcfd-step1.aspx' class='cta-btn green APPEND_MARKETING_CID_COOKIE' target='_blank'>Create Account</a>" ); 
