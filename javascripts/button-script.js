6cc23e03c85777acba37b86020fb95ac910102d5
manningtone <ewanmannington@live.co.uk>
2018-11-20 14:35:12 +0000

    Update button-script.js

--- javascripts/button-script.js
+++ javascripts/button-script.js
@@ -1,1 +1,3 @@
-$(".dform button.form-submit").after( "<a href='https://applyforanaccount.cityindex.co.uk/ciuk/sbcfd-step1.aspx' class='cta-btn green APPEND_MARKETING_CID_COOKIE' target='_blank'>Create Account</a>" ); 
+$(document).ready(function() {
+$(".dform button.form-submit").after( "<a href='https://applyforanaccount.cityindex.co.uk/ciuk/sbcfd-step1.aspx' class='cta-btn green APPEND_MARKETING_CID_COOKIE' target='_blank'>Create Account</a>" );
+});
